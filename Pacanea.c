ff037eee8ff14b36d63b5be3c125974d0d1acb46
Eroaleee <123071250+Eroaleee@users.noreply.github.com>
2023-01-19 12:15:29 +0200

    Update Pacanea.c

--- Pacanea.c
+++ Pacanea.c
@@ -3,9 +3,20 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
 #include <unistd.h>
 
-// sa rezolv  ctime
+
+void doSleep(int sec){
+     #ifdef _WIN32
+         Sleep(sec);
+        #else 
+            usleep(sec*1000*1000);
+         #endif
+ 
+}
 
 void PrintBani(int bani){
     printf("Mai ai : %d lei\n\n\n\n", bani);
@@ -22,17 +33,17 @@ void GameMenu(){
 }
 
 int Camatari(){
-
+    
     int bani_camatari;
-
+    
         printf("Ai ramas fara bani, daca vrei sa te imprumuti la CAMATARI spune-le suma pe care o vrei\n Daca nu, apasa 0\n");
         scanf("%d", &bani_camatari);
         return bani_camatari;
-
+    
 }
 
 int Dublaj(int bani, int contor, int pierdere, int castig){
-
+   
    srand(time(NULL));
    int RsauN, alegereaTa;
    PrintBani(bani);
@@ -42,8 +53,8 @@ int Dublaj(int bani, int contor, int pierdere, int castig){
 
    if(alegereaTa==1)
     return bani;
-
-
+   
+   
    if(alegereaTa==0){
 
       printf("Pentru ROSU alege 0, pentru NEGRU alege 1\n");
@@ -51,80 +62,87 @@ int Dublaj(int bani, int contor, int pierdere, int castig){
 
       RsauN=rand()%2;
 
-     if(RsauN==1)
+     if(RsauN==1){
+        doSleep(1);
          printf("\nNEGRU\n");
-     if(RsauN==0)
+        fflush(stdout); 
+         }
+     if(RsauN==0){
+       doSleep(1);
         printf("\nROSU\n");
-
+       fflush(stdout);
+        }
       if(alegereaTa==RsauN){
         printf("BRAVO, AI CASTIGAT\n");
         contor++;
         bani= pierdere + castig* pow(2, contor);
-
+        
        return Dublaj(bani, contor, pierdere, castig);
       }
 
 
      if(alegereaTa!=RsauN){
+        doSleep(1);
         printf("Ai Pierdut\n");
+        fflush(stdout);
         bani=pierdere;
         return bani;
      }
 
    }
 
-
+ 
 }
 
-int Pacanea( int banii ){
+int Pacanea( int bani ){
 
     srand(time(NULL));
     int nr1, nr2, nr3;
     int contorDublaj=0;
     int castig=0;
     int BaniInainteDeDublaj=0;
-
+    
     nr1=(int)rand()%10;
     nr2=(int)rand()%10;
     nr3=(int)rand()%10;
-
-     //printf("\n\n\n                                               ");
-     //sleep(10);
-     printf("%d  ", nr2);
-     sleep(3);
+     
+     printf("\n\n\n                                               ");
+     printf("%d ", nr1);
      fflush(stdout);
-     printf("%d  ", nr1);
-     sleep(3);
+     doSleep(1);
+     printf("%d ", nr2);
      fflush(stdout);
+     doSleep(1);
      printf("%d", nr3);
+     fflush(stdout);
      printf("  \n\n");
-
+    
         if(nr1==nr2 && nr1==nr3 && nr1==7){
             printf("                                            JACKPOT\n");
             castig=7000;
-            BaniInainteDeDublaj=banii;
-            banii+=castig;
-            banii=Dublaj(banii, contorDublaj, BaniInainteDeDublaj, castig);
+            BaniInainteDeDublaj=bani;
+            bani+=castig;
+            bani=Dublaj(bani, contorDublaj, BaniInainteDeDublaj, castig);
         }
-        else if(nr1==nr2 && nr1==nr3){
-            printf("                                 BRAVO, AI CASTIGAT 1000 DE LEI\n");
-           castig=1000;
-            BaniInainteDeDublaj=banii;
-            banii+=castig;
-            banii=Dublaj(banii, contorDublaj, BaniInainteDeDublaj, castig);
-        }
-        else if(nr1==nr2 || nr2==nr3 || nr3==nr1 ){
-             castig=5;
-            BaniInainteDeDublaj=banii;
-            banii+=castig;
-            banii=Dublaj(banii, contorDublaj, BaniInainteDeDublaj, castig);
-        }
+            else if(nr1==nr2 && nr1==nr3){
+                printf("                                 BRAVO, AI CASTIGAT 1000 DE LEI\n");
+            castig=1000;
+                BaniInainteDeDublaj=bani;
+                bani+=castig;
+                bani=Dublaj(bani, contorDublaj, BaniInainteDeDublaj, castig);
+            }
+                else if(nr1==nr2 || nr2==nr3 || nr3==nr1 ){
+                    castig=5;
+                    BaniInainteDeDublaj=bani;
+                    bani+=castig;
+                    bani=Dublaj(bani, contorDublaj, BaniInainteDeDublaj, castig);
+                }
 
+        
 
-
-
-
-        return banii;
+    
+        
+        return bani;
 }
 
 
@@ -134,7 +152,7 @@ int main(){
 
   printf("\n\n\nIntrodu suma de bani cu care intrii in cazino\n");
   scanf("%d", &BaniiTai);
-
+  
 
   while(jocul==1 && BaniiTai>=10){
 
@@ -160,7 +178,7 @@ int main(){
 
 
     if(contor!=0){
-        printf("Ai iesit din cazino cu %d lei si cu o datorie de %d la camatari.\n Daca nu o platesti in 10 zile o sa vina dupa tine! ", BaniiTai, camata);
+        printf("Ai iesit din cazino cu %d lei si cu o datorie de %d lei la camatari.\n Daca nu o platesti in 10 zile o sa vina dupa tine! ", BaniiTai, camata);
     }
     else{
 
