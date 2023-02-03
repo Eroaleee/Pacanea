8276b2ab799acd064ea52ec7ab63d424df73e6a6
Eroaleee <123071250+Eroaleee@users.noreply.github.com>
2023-02-03 22:00:36 +0200

    Delete functionarepacanea.h

--- functionarepacanea.h
+++
@@ -1,7 +0,0 @@
-#ifndef FUNCTIONAREPACANEA
-#define FUNCTIONAREPACANEA
-
-int dubleazaBanii(int bani, int contor, int pierdere, int castig);
-int utilizarePacanea( int bani );
-
-#endif //FUNCTIONAREPACANEA
